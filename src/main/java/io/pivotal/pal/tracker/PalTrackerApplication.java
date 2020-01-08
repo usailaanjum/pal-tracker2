5d49a993b442918a73e9da1de3421b10fd35cf49
Bill Kable <bkable@pivotal.io>
2020-01-08 11:09:21 -0500

    adding the correct solution

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,8 +1,16 @@
 package io.pivotal.pal.tracker;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+<<<<<<< HEAD
+=======
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+>>>>>>> 65651fe... Add TimeEntry MVC in memory
 
 @SpringBootApplication
 public class PalTrackerApplication {
@@ -13,6 +21,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
+<<<<<<< HEAD
     public TimeEntryRepository timeEntryRepository(){
         return timeEntryRepository();
     }
@@ -20,4 +29,9 @@ public class PalTrackerApplication {
 
 
 
+=======
+    TimeEntryRepository timeEntryRepository() {
+        return new InMemoryTimeEntryRepository();
+    }
+>>>>>>> 65651fe... Add TimeEntry MVC in memory
 }
