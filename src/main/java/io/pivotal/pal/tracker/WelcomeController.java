5e03dc9a737f7d34cfee39fccf3e6b0af3018d08
Usaila Anjum <usailaanjum@gmail.com>
2020-03-07 10:16:14 -0500

    Adding a manifest to the tracker app

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,23 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String message;
+
+//we are configuring to provide our app with environment variables when it is executed
+//@Value takes a specific format to reference an environment variable
+//usually on a constructor
+    public WelcomeController(@Value("${welcome.message}") String message){
+
+        this.message = message;
+    }
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
